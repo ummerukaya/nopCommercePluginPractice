955e79d59fb6756dca7db6a7b7089f0f6b2c26bc
ummerukaya <ummerukaya13@gmail.com>
2022-02-28 19:59:16 +0600

    Plugin added

---
+++ src/Plugins/Nop.Plugin.Misc.LoginManager/LoginManagerPlugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Core.Domain.Cms;
+using Nop.Services.Common;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Plugins;
+using Nop.Services.Stores;
+
+namespace Nop.Plugin.Misc.LoginManager
+{
+    public class LoginManagerPlugin : BasePlugin, IMiscPlugin
+    {
+       
+        private readonly ILocalizationService _localizationService;
+        private readonly ISettingService _settingService;
+        private readonly IStoreService _storeService;
+        private readonly IWebHelper _webHelper;
+        private readonly WidgetSettings _widgetSettings;
+
+        public LoginManagerPlugin(IGenericAttributeService genericAttributeService,
+            ILocalizationService localizationService,
+            ISettingService settingService,
+            IStoreService storeService,
+            IWebHelper webHelper,
+            WidgetSettings widgetSettings)
+        {
+           _localizationService = localizationService;
+           _settingService = settingService;
+            _storeService = storeService;
+            _webHelper = webHelper;
+            _widgetSettings = widgetSettings;
+        }
+
+       
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/LoginManager/Configure";
+        }
+
+        public override async Task InstallAsync()
+        {    
+            await base.InstallAsync();
+        }
+
+       
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+
+       
+    }
+}
